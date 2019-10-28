9e7ce75dcf39568268a974552b1b2d661085a83a
Joshua Astray <joshuasrkf@gmail.com>
2019-10-28 23:24:10 +0800

    feat: add CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,13 +3,14 @@
 import connexion
 
 from swagger_server import encoder
-
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.app.secret_key = "super_secret_key"
     app.add_api('swagger.yaml', arguments={'title': 'To Love Webcasts'}, pythonic_params=True)
+    CORS(app.app)
     app.run(port=8080)
 
 
